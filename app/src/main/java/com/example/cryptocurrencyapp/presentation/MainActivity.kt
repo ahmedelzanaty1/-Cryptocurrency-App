218b453c9a7eb3ec5ee672b66c903428e2721301
Hp <ahmedelzanaty001@gmail.com>
2025-03-11 17:01:44 +0200

    Creating the repository "Fixed"

--- app/src/main/java/com/example/cryptocurrencyapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cryptocurrencyapp/presentation/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cryptocurrencyapp.presentation.theme.CryptocurrencyAppTheme
@@ -21,7 +24,6 @@ class MainActivity : ComponentActivity() {
             CryptocurrencyAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +33,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting( modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello Still Working !",
         modifier = modifier
     )
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        CircularProgressIndicator()
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CryptocurrencyAppTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
